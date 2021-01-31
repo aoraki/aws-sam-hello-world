2ca3eac3fe08ac389f17af1549e694669953169a
John Kelly <kellyjra@gmail.com>
2021-01-31 16:51:19 +0000

    chore: fixing broken test

--- HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest {
     String content = result.getBody();
     assertNotNull(content);
     assertTrue(content.contains("\"message\""));
-    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"hello world, testing CI/CD\""));
     assertTrue(content.contains("\"location\""));
   }
 }
