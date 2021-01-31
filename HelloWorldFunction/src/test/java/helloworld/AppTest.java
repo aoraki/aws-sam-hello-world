026a113c3e72e3d4075e69f2be65f8a03d8a6c43
John Kelly <kellyjra@gmail.com>
2021-01-31 20:57:27 +0000

    chore: change in message to test the idempotency of the CI/CD deployment process (AWS SAM)

--- HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest {
     String content = result.getBody();
     assertNotNull(content);
     assertTrue(content.contains("\"message\""));
-    assertTrue(content.contains("\"hello world, testing CI/CD\""));
+    assertTrue(content.contains("\"hello world, testing CI/CD NEW\""));
     assertTrue(content.contains("\"location\""));
   }
 }
