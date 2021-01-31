b59a1fb243dc7bbcacdb324dfeb07589a19d2458
John Kelly <kellyjra@gmail.com>
2021-01-31 16:48:38 +0000

    chore: Updating the output message to test the CI/CD pipeline

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -27,7 +27,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("{ \"message\": \"hello world, testing CI/CD \", \"location\": \"%s\" }", pageContents);
 
             return response
                     .withStatusCode(200)
